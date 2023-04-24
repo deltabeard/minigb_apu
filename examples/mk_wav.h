4ab2f34157ba80c3d6af414b69ae5f2cf8ff274a
Mahyar Koshkouei <mk@deltabeard.com>
2023-04-24 11:34:32 +0100

    example: add comments and licence to mk_wav
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- examples/mk_wav.h
+++ examples/mk_wav.h
@@ -1,3 +1,15 @@
+/**
+ * Simple WAV writer for little endian computers.
+ * Copyright (c) 2023 Mahyar Koshkouei
+ *
+ * Licence: Public Domain
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ * THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED WARRANTY.
+ * IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES ARISING FROM THE
+ * USE OF THIS SOFTWARE.
+ */
+
 #pragma once
 
 #include <assert.h>
@@ -8,16 +20,34 @@ struct mk_wav_ctx {
 	size_t bytes;
 };
 
-/* Initialise WAV writer library. */
+/**
+ * Initialise WAV writer library. FILE must already be opened and seekable.
+ *
+ * \param c Pointer to a mk_wav context.
+ * \param out Pointer to opened and writable file pointer.
+ * \param bits_per_sample
+ * \param channels
+ * \param sample_rate
+ * \returns 0 on success, negative on error.
+ */
 int mk_wav_init(struct mk_wav_ctx *c, FILE *out, unsigned short bits_per_sample,
 		unsigned short channels, unsigned long sample_rate);
 
-/* Write audio samples to output file. */
+/**
+ * Write audio samples to output file.
+ * \param c An initialised mk_wav context.
+ * \param samples Data samples to write to output WAV file.
+ * \param len Number of bytes in samples.
+ * \returns bytes written; equal to len on success.
+ */
 size_t mk_wav_write(struct mk_wav_ctx *c, void *samples, size_t len);
 
-/* Exit WAV writer library. FILE is not closed. */
+/**
+ * Updates the file size in the WAV header. FILE is not closed.
+ * \param c An initialised mk_wav context.
+ * \returns 0 on success, negative on error.
+ */
 long mk_wav_exit(struct mk_wav_ctx *c);
-
 
 int mk_wav_init(struct mk_wav_ctx *c, FILE *out, unsigned short bits_per_sample,
 		unsigned short channels, unsigned long sample_rate)
@@ -32,6 +62,7 @@ int mk_wav_init(struct mk_wav_ctx *c, FILE *out, unsigned short bits_per_sample,
 	c->out = out;
 	c->bytes = 0;
 
+	/* Write file headers. */
 	if(fwrite(hdr, 1, sizeof(hdr), out) < sizeof(hdr))
 		goto out;
 
@@ -74,6 +105,8 @@ size_t mk_wav_write(struct mk_wav_ctx *c, void *samples, size_t len)
 	assert(c != NULL);
 	assert(c->out != NULL);
 	assert(samples != NULL);
+
+	/* Write samples directly to WAV file. */
 	return fwrite(samples, 1, len, c->out);
 }
 
@@ -86,16 +119,24 @@ long mk_wav_exit(struct mk_wav_ctx *c)
 
 	assert(c != NULL);
 	assert(c->out != NULL);
+
+	/* Obtain file size. */
 	sz = ftell(c->out);
 	if(sz < 0)
 		return sz;
 
+	/* Calculate size of RIFF and DATA chunks. */
 	riff_sz = sz - 8ul;
 	data_sz = sz - 78ul;
+
+	/* Update RIFF and DATA chunk sizes in WAV header. */
 	fseek(c->out, WAV_RIFF_SIZE_LOC, SEEK_SET);
 	fwrite(&riff_sz, 4, 1, c->out);
 	fseek(c->out, WAV_DATA_SIZE_LOC, SEEK_SET);
 	fwrite(&data_sz, 4, 1, c->out);
 
+	/* Seek to end of file and return. */
 	return fseek(c->out, 0, SEEK_END);
+#undef WAV_RIFF_SIZE_LOC
+#undef WAV_DATA_SIZE_LOC
 }
