e3ab29b2c7adcf607759300b681595a3e16f2c88
Mahyar Koshkouei <mk@deltabeard.com>
2025-01-11 18:07:37 +0000

    rp2040: simplify benchmark init
    48MHz sys clock and the getchar timeout functions do not seem to work
    correctly on the RP2350.
    
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- benchmark-rp2040/benchmark.c
+++ benchmark-rp2040/benchmark.c
@@ -46,30 +46,21 @@ void process_apu(void)
 void main(void)
 {
 	/* Use an accurate clock. */
-	set_sys_clock_48mhz();
+	//set_sys_clock_48mhz();
 
 	stdio_init_all();
 
 	while(1) {
 		uint32_t bef, aft;
 		uint32_t interrupts;
-		while(getchar_timeout_us(1 * 1000 * 1000) == PICO_ERROR_TIMEOUT) {
-			static unsigned wrap = 0;
-			putchar_raw('.');
-			wrap++;
-			if(wrap >= 16) {
-				putchar_raw('\n');
-				wrap = 0;
-			}
-		}
 
-		printf("Starting benchmark with sample rate %lu\n", AUDIO_SAMPLE_RATE);
 		interrupts = save_and_disable_interrupts();
 		bef = time_us_32();
 		process_apu();
 		aft = time_us_32();
 		restore_interrupts(interrupts);
-		printf("Duration: %lu us\n", aft - bef);
+
+		printf("%lu us\n", aft - bef);
 	}
 
 	return;
